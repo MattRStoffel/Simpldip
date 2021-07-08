7a3e345f8a1459caedff06834c5eb186b2b3b6c0
Matthew Stoffel <mrstoffel@me.com>
2021-07-08 10:11:59 -0700

    Initial Commit

---
+++ SimplDip/SimplDipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplDipApp.swift
+//  SimplDip
+//
+//  Created by Matt Stoffel on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SimplDipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
